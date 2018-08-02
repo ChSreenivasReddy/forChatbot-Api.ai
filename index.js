a3d7b48895733e78bded872c074cdbe358b69b45
ChSreenivasReddy <41984827+ChSreenivasReddy@users.noreply.github.com>
2018-08-02 16:52:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ res.setHeader('Content-Type','application/json');
 	var response="";
     return res.json({
                    "fulfillmentText": response,
-                   "fulfillmentMessages": [{"text": {"text": ["Hello Welcome to Chatbot !!! This is an Webhook API call for Claims....."]}}],
+                   "fulfillmentMessages": [{"text": {"text": [msg]}}],
 				   "source":""
 				  });
 });
