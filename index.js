3ab3d6072819ca855f9601307069224588283ed4
ChSreenivasReddy <41984827+ChSreenivasReddy@users.noreply.github.com>
2018-08-04 23:04:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const bodyParser = require("body-parser");
 
 const restService = express();
 
-var request=require("request");
-var path=require("path");
-
 restService.use(
   bodyParser.urlencoded({
     extended: true
@@ -16,44 +13,15 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-
-var result;
-function cb (error, response, body){
-  if(error){
-  console.log('error',error);
-  }
-   var resApi=JSON.parse(body);
-   if(resApi.message==''){
-   }
-   else
-   {
-   result=resApi;
-    }
-   
-}
-
-function getClaim(){
-result=undefined;
-//var url=""+identifier;
-var url="http://api.wunderground.com/api/Your_Key/conditions/q/CA/San_Francisco.json";
-console.log(url);
-var req=request(url,cb);
-while(result===undefined){
-  require('deasync').runLoopOnce();
-  }
-return result;
-}
-
 restService.post('/echo', function(req, res) {
 res.setHeader('Content-Type','application/json');
-    var msg = getclaim();
-  var response="";
-	return msg;
-   // return res.json({
+    var msg = req.body.queryResult.parameters['echoText'];
+	var response="";
+    return res.json({
                    "fulfillmentText": response,
-                   "fulfillmentMessages": [{"text": {"text": ["Report A Damage!!! This is an Webhook API call for Claims....."]}}],
-           "source":""
-          });
+                   "fulfillmentMessages": [{"text": {"text": ["Report A Damage!!! Loss Types -->This is an Webhook API call for Claims....."]}}],
+				   "source":""
+				  });
 });
 
 restService.listen((process.env.PORT || 8000), function() {
