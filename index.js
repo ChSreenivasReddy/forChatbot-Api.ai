5ee74520b1cff8ab1188e4b72cdbbdd4501f3c90
ChSreenivasReddy <41984827+ChSreenivasReddy@users.noreply.github.com>
2018-08-07 18:42:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-"use strict";
-
-const express = require("express");
-const bodyParser = require("body-parser");
-
-const restService = express();
-
+var express = require("express");
+var bodyParser = require("body-parser");
+var https = require('https');
+var restService = express();
+var resStatus;
 restService.use(
   bodyParser.urlencoded({
     extended: true
@@ -13,16 +11,41 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post('/echo', function(req, res) {
-res.setHeader('Content-Type','application/json');
-    var msg = req.body.queryResult.parameters['echoText'];
-	var response="";
-    return res.json({
-                   "fulfillmentText": response,
-                   "fulfillmentMessages": [{"text": {"text": ["Loss Types:Employee Occupational,Employee Accident,Employee Leisure,Enterpreneur Accident,Enterpreneur Occupational,Enterpreneur Leisure,Student Declaration"]}}],
+
+restService.post('/echo', function(request, response) {
+response.setHeader('Content-Type','application/json');
+    //var msg = req.body.queryResult.parameters['echoText'];
+	
+console.log("POST request to the Node.js method:start");
+
+var options = {
+  host: 'en.wikipedia.org',
+  port: 443,
+  path: '/wiki/A._P._J._Abdul_Kalam',
+  method: 'POST'
+};
+
+https.request(options, function(res) {
+  console.log('HTTPS Status STATUS:' + res.statusCode);
+  resStatus='Fullfillmentmessage';
+  //console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    //console.log('BODY: ' + chunk);
+	resStatus='fulfillmentText';
+  });
+}).end();
+console.log("POST request to the Node.js method:End");
+var responseData="";
+    var resJson= response.json({
+                   "fulfillmentText": responseData,
+                   "fulfillmentMessages": [{"text": {"text": [resStatus]}}],
 				   "source":""
 				  });
+				  //console.log("Response JSON "+resJson)
 });
+
+
 
 restService.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
